00c5a4f241cb5398f815ad60a796a5f26a76bd8e
nyanyaww <515478826@qq.com>
2019-02-03 21:06:36 +0800

    动态分配内存

--- dynatic_memery/main.c
+++ dynatic_memery/main.c
@@ -1,9 +1,17 @@
-#include <stdio.h>
+#define LONG 10
+#include <string.h>
 #include <stdlib.h>
-#include <assert.h>
-
-int main(void)
+#include <stdio.h>
+main()
 {
-    printf("%d", NULL);
+    char *message, buf[LONG];
+    gets(buf);
+    message = (char *)realloc(NULL, strlen(buf) + 1);
+    strcpy(message, buf);
+    puts(message);
+    gets(buf);
+    message = (char *)realloc(message, (strlen(message) + strlen(buf) + 1));
+    strcat(message, buf);
+    puts(message);
     return 0;
 }
