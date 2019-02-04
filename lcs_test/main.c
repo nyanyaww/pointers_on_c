773a629068555476b31a2cd7102a871710b7bb3e
李聪盛 <515478826@qq.com>
2019-02-04 22:36:56 +0800

    服务器和客户端

--- lcs_test/main.c
+++ lcs_test/main.c
@@ -1,18 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 #include "Error/error.h"
 #include "DynamicMemeryAllocation/myDMA.h"
 #include "FileOperate.h"
 #include "Log/log.h"
 
+#define DEBUG !0
+
+#if DEBUG
+
 int main(int argc, char const *argv[])
 {
-    FILE *fp = fopen("test.log", "a+");
-    if (argc != 2)
-    {
-        writeTime();
-        CODE_ERROR(fp, SOME_ERROR, some_error_code);
-    }
+    // FILE *fp = fopen("test.log", "a+");
+    // if (argc != 2)
+    // {
+    //     writeTime();
+    //     CODE_ERROR(fp, SOME_ERROR, some_error_code);
+    // }
     return 0;
 }
+#endif
