756001b65dd942f9424468ac80cd0124765bdf9f
李聪盛 <515478826@qq.com>
2019-02-04 23:21:24 +0800

    文件读取完成

--- lcs_test/main.c
+++ lcs_test/main.c
@@ -8,17 +8,26 @@
 #include "Log/log.h"
 
 #define DEBUG !0
-
+#define MAX_LINE 80
 #if DEBUG
 
 int main(int argc, char const *argv[])
 {
-    // FILE *fp = fopen("test.log", "a+");
-    // if (argc != 2)
-    // {
-    //     writeTime();
-    //     CODE_ERROR(fp, SOME_ERROR, some_error_code);
-    // }
+    char buff[255];
+    int len = 0;
+    int line = 0;
+    FILE *fp = fopen("conf.ini", "r");
+    while (fgets(buff, MAX_LINE, fp) != NULL)
+    {
+        line += 1;
+        len = strlen(buff);
+        if (buff[0] != '\n')
+        {
+            if (buff[len - 1] == '\n')
+                buff[len - 1] = '\0';
+            printf("line %d :%s\n", line, buff);
+        }
+    }
     return 0;
 }
 #endif
